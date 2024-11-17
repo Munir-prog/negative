52892751829c4a9d07f4b67f50162c6cfbf3222d
Munir-prog <munirprog@gmail.com>
2024-11-17 11:41:10 +0300

    init

---
+++ src/main/java/com/mprog/negative/NegativeApplication.java
@@ -0,0 +1,35 @@
+package com.mprog.negative;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@SpringBootApplication
+@RequestMapping("/api/negative")
+public class NegativeApplication {
+
+//	@Value("${spring.cloud.discovery.instance-id}")
+//	private String service;
+//
+
+	public static void main(String[] args) {
+		SpringApplication.run(NegativeApplication.class, args);
+	}
+
+	record Negative(int id, int reliability, String text) {
+	}
+
+	@GetMapping
+	public List<Negative> getNegatives() {
+//		log.info("service: {}", service);
+		return List.of(new Negative(1, 20, "Bank robbery"), new Negative(2, 85, "Mall robbery"));
+	}
+}
---
+++ src/test/java/com/mprog/negative/NegativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mprog.negative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NegativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
