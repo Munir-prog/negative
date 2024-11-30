64166c960582447d8123ceafcbc82cbc477bcdbd
Munir-prog <munirprog@gmail.com>
2024-11-30 21:11:54 +0300

    add app registration

--- src/main/java/com/mprog/negative/NegativeRepository.java
+++ src/main/java/com/mprog/negative/NegativeRepository.java
@@ -1,10 +1,20 @@
 package com.mprog.negative;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @Repository
 public class NegativeRepository {
@@ -27,5 +37,25 @@ public class NegativeRepository {
                 .setParameter("count", count + 1)
                 .setParameter("name", serviceName)
                 .getSingleResult();
+    }
+
+
+    @Autowired
+    @Qualifier("transactionManager")
+    protected PlatformTransactionManager txManager;
+
+    @PostConstruct
+    private void init() throws UnknownHostException {
+        String serviceName = InetAddress.getLocalHost().getHostName();
+        TransactionTemplate tmpl = new TransactionTemplate(txManager);
+        tmpl.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus status) {
+                entityManager.createNativeQuery("insert into request (name, count)\n" +
+                                "values (:name, 0)")
+                        .setParameter("name", serviceName)
+                        .executeUpdate();
+            }
+        });
     }
 }
